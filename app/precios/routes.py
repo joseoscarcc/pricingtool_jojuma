2fb56423793df6e133a4fd81275959c92bd23b64
joseoscarcc <94883708+joseoscarcc@users.noreply.github.com>
2023-07-24 17:54:31 -0600

    Update routes.py
    changes made, to make possible filter by cre_id in table prices

--- app/precios/routes.py
+++ app/precios/routes.py
@@ -1,14 +1,21 @@
-from flask import render_template
+from flask import render_template, request
 from app.precios import bp
 from app.models.auth import login_required
-from app.models.precios import get_data_table
+from app.models.precios import get_data_table, get_competencia_by_place_id
+import json
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 @login_required
-def index():
+def index(target_cre_id=None):
+    title="Tabla Precios"
+    if request.method == 'POST':
+        target_cre_id = request.form.get('target_cre_id')
     
-    latest = get_data_table()
-    return render_template('precios/index.html', latest=latest)
+    print(target_cre_id)
+    latest = get_data_table(target_cre_id)
+    #test = get_competencia_by_place_id('640')
+    #print(test)
+    return render_template('precios/index.html', title=title,latest=latest)
 
 @bp.route('cambios/')
 @login_required
